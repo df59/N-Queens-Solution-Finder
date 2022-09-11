0b91b5dbbd957e33775b1b92f4289c4f6f78ac69
Dustin Franklin <df59@zips.uakron.edu>
2022-09-11 03:02:28 -0400

    Clean up file input

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
-#include <ios>
-#include <iostream>
-#include <fstream>
-#include <math.h>
-#include <ostream>
-#include <pthread.h>
-#include <string>
-#include <type_traits>
+#include <cassert>
 #include <cmath>
+#include <cstddef>
+#include <exception>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+#include <string>
 #include <vector>
 
 int getFileSize();
@@ -15,165 +15,119 @@ int resizeArray();
 int* fillResized(int*, int*, int);
 
 
-
 class Board {
-    private:
-    std::vector<int> state;
-    int width;
-    public:
-    Board(std::vector<int> in_Board) {
-        state = in_Board;
-        width = sqrt(in_Board.size());
-    }
+  private:
+	std::vector<std::byte> state;
+	std::size_t m_width = 0U;
 
-    ~Board() {
-        state.clear();
-        state.shrink_to_fit();
-    }
+  public:
+	explicit Board(std::vector<std::byte>& in_Board, std::size_t const width)
+		: state(in_Board), m_width(width) {
+		assert(m_width * m_width == state.size());
+	}
 
-    int Width() const {
-        return width;
-    }
+	explicit Board() = default;
 
-    int& operator[](unsigned int index) {
-        return state[index];
-    }
+	[[nodiscard]] std::size_t
+	Width() const {
+		return m_width;
+	}
 
-    friend std::ostream& operator<<(std::ostream& stream, const Board& board);
+	void
+	clear() {
+		state.clear();
+		m_width = 0U;
+	}
 
+	std::byte&
+	operator[](unsigned int index) {
+		return state[index];
+	}
 
+	std::byte
+	operator[](unsigned int index) const {
+		return state[index];
+	}
+
+	friend std::istream& operator>>(std::istream& is, Board& board);
+	friend std::ostream& operator<<(std::ostream& stream, const Board& board);
 };
 
+std::istream&
+operator>>(std::istream& is, Board& board) {
+	board.clear();
+	auto line = std::string{};
+	assert(board.m_width == 0U);
+	auto size_Of_Board = 0UL;
 
+	while (!is.eof()) {
+		auto c = is.get();
+		if (c == ',') continue;
+		if (c == '\n' || c == '\r') {
+			if (board.m_width == 0U) {
+				board.m_width = board.state.size();
+				size_Of_Board = board.m_width * board.m_width;
+				board.state.reserve(board.m_width * board.m_width);
+			}
+			continue;
+		}
 
-std::ostream& operator<<(std::ostream& stream, const Board& board) {
-    //Debug std::cout << "output stream of Board.state: \n";
-    for(int i = 0; i<board.Width(); i++) {
-    for(int j = board.Width()*i; j<board.Width()*(i+1); j++) {
-    stream << board.state[j] << " | ";
-    }
-    std::cout << '\n';
-    }
-return stream;
+		if (c != '0' && c != '1') {
+			is.setstate(std::ios::failbit);
+			return is;
+		}
+		board.state.push_back(static_cast<std::byte>(c != '0'));
+		if (board.state.size() == size_Of_Board) {
+			break;
+		}
+	}
+
+	return is;
 }
 
-
-
-int main() {
-    int File_Size = getFileSize();
-    std::cout << "file size is " << File_Size << " characters \n";
-
-    int a[File_Size];                       //allocating array in the scope of main
-    int *raw_Input = getRawInput(a);
-    int reduced_size = resizeArray();
-    std::cout << "new size is " << reduced_size << '\n';
-
-    int q[reduced_size];
-    int *reduced_Input = fillResized(raw_Input, q, reduced_size);
-
-    /*Debug std::cout << "from function 'main' reduced size array: \n";
-    for(int i = 0; i < reduced_size; i++) {
-        std::cout << "position " << i << " is the number " << reduced_Input[i] << "\n";
-    }
-    */
-
-    int width = sqrt(reduced_size);
-    std::vector<int> input_Vector;
-    //Debug std::cout << "inputting array into std::vector: \n";
-    for(int i = 0; i < reduced_size; i++) {
-    input_Vector.push_back(reduced_Input[i]);
-    //Debug std::cout << input_Vector[i] << '\n';
-    }
-
-    Board start_State = input_Vector;
-    std::cout << "The starting board is: \n" << start_State;
-
-    std::cout << "Board object start_State width is: \n" << start_State.Width() << '\n';
-
+std::ostream&
+operator<<(std::ostream& stream, const Board& board) {
+	stream << "output stream of Board.state: \n";
+	stream << "Board.state size: \n";
+	stream << board.state.size() << '\n';
+	for (auto c : board.state) {
+		stream << static_cast<int>(c) << ' ';
+	}
+	stream << '\n';
+	for (auto i = 0U; i < board.Width(); i++) {
+		for (auto j = board.Width() * i; j < board.Width() * (i + 1); j++) {
+			stream << static_cast<int>(board.state[j]) << " | ";
+		}
+		stream << '\n';
+	}
+	return stream;
 }
 
-int getFileSize() {
-    //opening file
-    std::cout << "Opening input file \n";
-    std::string csv_File = "input.csv";  //input file must be within project directory
-    std::ifstream in_File;
-    in_File.open(csv_File.c_str(), std::ios::in);
+[[nodiscard]] std::stringstream
+getFile(std::string const input_file) {
+	auto i_filestream = std::ifstream{input_file.c_str()};
 
-    if(!in_File.is_open()) {
-        std::cout << "File did not open. Make sure 'input.csv' is in the project directory. \n";
-    }
-//finding file size to size array
-    std::streampos file_Size = 0;
-    file_Size = in_File.tellg();
-    in_File.seekg(0, std::ios::end);
-    file_Size = in_File.tellg() - file_Size;
+	if (!i_filestream.is_open()) {
+		throw std::runtime_error(
+			"File did not open. Make sure it is in the project directory \n");
+	}
+	auto ss = std::stringstream();
+	i_filestream.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+	ss << i_filestream.rdbuf();
 
-    std::cout << "Closing input file \n";
-    in_File.close();
-    return int(file_Size);
+	return ss;
 }
 
-int* getRawInput(int input[]) {
-    //opening file
-    std::cout << "Opening input file \n";
-    std::string csv_File = "input.csv";  //input file must be within project directory
-    std::ifstream in_File;
-    in_File.open(csv_File.c_str(), std::ios::in);
-
-    if(!in_File.is_open()) {
-        std::cout << "File did not open. Make sure 'input.csv' is in the project directory. \n";
-    }
-
-    in_File.seekg(0);
-    int position = 0;
-    char c;
-//filling array with raw input from file
-    while(in_File.get(c)) {
-    if((int(c) - int('0')) < 10) {
-        if((int(c) - int('0')) >=0) {
-    input[position] = (int(c) - int('0')); //subtracting ascii 0 from ascii(c) to convert ascii code to real int value
-    //std::cout << "position " << position + 1 << " is the number " << input[position] << "\n";
-    position++;
-        }
-    }
-    }
-    return input;
-}
-
-
-
-int resizeArray() {
-
-    //opening file
-    std::cout << "Opening input file \n";
-    std::string csv_File = "input.csv";  //input file must be within project directory
-    std::ifstream in_File;
-    in_File.open(csv_File.c_str(), std::ios::in);
-
-    if(!in_File.is_open()) {
-        std::cout << "File did not open. Make sure 'input.csv' is in the project directory. \n";
-    }
-
-    in_File.seekg(0);
-    int new_Size = 0;
-    char c;
-    while(in_File.get(c)) {
-    if((int(c) - int('0')) < 10) {
-        if((int(c) - int('0')) >=0) {
-    new_Size++;
-        }
-    }
-    }
-
-    return new_Size;
-}
-
-int* fillResized(int old[], int resized[], int new_Size) {
-    //std::cout << "from function 'fillResized': \n";
-    for(int i = 0; i < new_Size; i++) {
-    resized[i] = old[i];
-    //std::cout << "position " << i << " is the number " << resized[i] << "\n";
-
-    }
-    return resized;
+int
+main() {
+	try {
+		auto content_Of_File = getFile("input.csv");
+		std::cout << "stringstream output: " << content_Of_File.str() << '\n';
+		auto board = Board{};
+		content_Of_File >> board;
+		std::cout << "======================================== \n";
+		std::cout << board;
+	} catch (std::exception& ex) {
+		std::cerr << ex.what();
+	}
 }
